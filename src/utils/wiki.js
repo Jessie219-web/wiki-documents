3882507543ad2f4657c955e1a65fdbeebdcbedc1
nfs0619 <1447996181@qq.com>
2025-04-28 13:50:59 +0800

    Update weeklywiki

--- src/utils/wiki.js
+++ src/utils/wiki.js
@@ -1,86 +1,86 @@
 const docList = [
   [
     {
+      "path": "/recomputer_j401b_getting_start",
+      "image": "https://files.seeedstudio.com/wiki/reComputer-Jetson/J401B/recomputer-j401b_1.webp",
+      "title": "Getting start with reComputer J401B",
+      "date": "4/27/2025"
+    },
+    {
+      "path": "/recomputer_j401b_interfaces_usage",
+      "image": "https://files.seeedstudio.com/wiki/reComputer-Jetson/J401B/j401b_interfaces.webp",
+      "title": "J401B Interfaces Usage",
+      "date": "4/27/2025"
+    }
+  ],
+  [
+    {
+      "path": "/WM1302_module",
+      "image": "https://files.seeedstudio.com/wiki/WM1302_module/WM1302_3.webp",
+      "title": "LoRaWAN® Gateway Module WM1302",
+      "date": "4/24/2025"
+    },
+    {
+      "path": "/total_solar_radiation_sensor",
+      "image": "https://files.seeedstudio.com/wiki/Total_Solar_Radiation_Sensor/image1.webp",
+      "title": "Total Solar Radiation Sensor",
+      "date": "4/23/2025"
+    }
+  ],
+  [
+    {
+      "path": "/flash_opensource_firmware_to_m2_gateway",
+      "image": "https://files.seeedstudio.com/wiki/SenseCAP/M2_Multi-Platform/m2-white.webp",
+      "title": "Flash Opensource Firmware to M2 Gateway",
+      "date": "4/22/2025"
+    },
+    {
       "path": "/Grove-Barometer-High-Accuracy",
       "image": "https://files.seeedstudio.com/wiki/Grove-Barometer-High-Accuracy/img/main.webp",
       "title": "Grove - Barometer (High-Accuracy)",
       "date": "4/22/2025"
-    },
+    }
+  ],
+  [
     {
       "path": "/respeaker_2_mics_pi_hat_raspberry_v2",
       "image": "https://files.seeedstudio.com/wiki/ReSpeaker_2_Mics_Pi_HAT/social-image.webp",
       "title": "Getting Started with Raspberry Pi",
       "date": "04/18/2025"
-    }
-  ],
-  [
+    },
     {
       "path": "/tracker_at_command",
       "image": "https://files.seeedstudio.com/wiki/SenseCAP/LoraWAN_Tracker/intro-e.webp",
       "title": "LoRaWAN Tracker Bluetooth AT Command Instruction",
       "date": "4/15/2025"
-    },
+    }
+  ],
+  [
     {
       "path": "/Grove-Air_Quality_Sensor_v1.3",
       "image": "https://files.seeedstudio.com/wiki/Grove_Air_Quality_Sensor_v1.3/img/Grove-Air_Quality_Sensor_v1.3.webp",
       "title": "Grove - Air Quality Sensor v1.3",
       "date": "4/14/2025"
-    }
-  ],
-  [
+    },
     {
       "path": "/Grove-Temperature_Humidity_Pressure_Gas_Sensor_BME680",
       "image": "https://files.seeedstudio.com/wiki/Grove-Temperature-Humidity-Pressure-Gas-Sensor_BME680/img/main.webp",
       "title": "Grove - Temperature Humidity Pressure Gas Sensor(BME680)",
       "date": "4/14/2025"
-    },
+    }
+  ],
+  [
     {
       "path": "/xiao-c3-ibeacon",
       "image": "https://files.seeedstudio.com/wiki/xiao-c3-ibeacon/8.webp",
       "title": "XIAO ESP32-C3 iBeacon with BME680 Sensor Using ESP-IDF",
       "date": "04/14/2025"
-    }
-  ],
-  [
+    },
     {
       "path": "/create_backup_and_restore_on_recomputer",
       "image": "https://files.seeedstudio.com/wiki/reComputer-Jetson/reComputer_backup/jtop2.webp",
       "title": "Create Backup and Restore on reComputer",
       "date": "04/11/2025"
-    },
-    {
-      "path": "/recamera_gimbal_getting_started",
-      "image": "https://files.seeedstudio.com/wiki/reCamera/Gimbal/reCamera-Gimbal.webp",
-      "title": "Getting Started with reCamera Gimbal",
-      "date": "04/09/2025"
-    }
-  ],
-  [
-    {
-      "path": "/recamera_gimbal_node_red",
-      "image": "https://files.seeedstudio.com/wiki/reCamera/Gimbal/workspace_flow.webp",
-      "title": "Node-RED Introduction for Gimbal",
-      "date": "04/09/2025"
-    },
-    {
-      "path": "/recamera_pid_adjustment",
-      "image": "https://files.seeedstudio.com/wiki/reCamera/Gimbal/PID_nodered.webp",
-      "title": "reCamera Gimbal PID Settings",
-      "date": "04/08/2025"
-    }
-  ],
-  [
-    {
-      "path": "/getting_started_with_seeed_iot_button",
-      "image": "https://files.seeedstudio.com/wiki/IoT_Botton_ESPHOME/button_esphome/button_buzzer.webp",
-      "title": "Getting Started with Seeed Studio IoT Button",
-      "date": "04/08/2025"
-    },
-    {
-      "path": "/xiao-esp32s3-nuttx",
-      "image": "https://files.seeedstudio.com/wiki/XIAO-nRF52840-NuttX/nuttx.webp",
-      "title": "Seeed Studio XIAO ESP32S3 with NuttX(RTOS)",
-      "date": "04/08/2025"
     }
   ]
 ];
