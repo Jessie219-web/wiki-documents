4b1f04997dbf18fd7cc7d5f0a2c1a455d89b714d
elainedanwu <254693273@qq.com>
2025-07-01 14:36:17 +0800

    add:phospho

--- sidebars.js
+++ sidebars.js
@@ -3993,6 +3993,7 @@ const sidebars = {
                   'Robotics/Robot_Software/Isaac_Lab/Install_Isaac_lab',
                     ]},
             'Robotics/Robot_Software/VLA/control_robotic_arm_via_gr00t',
+            'Robotics/Robot_Software/control_robotic_arm_via_phospho',
           ]
 
     },
