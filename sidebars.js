2eeb087af54cdf56464bfba893dea5336fb35425
KasunThushara <73091563+KasunThushara@users.noreply.github.com>
2025-07-16 11:34:54 +0800

    change sidebar.js

--- sidebars.js
+++ sidebars.js
@@ -1934,7 +1934,15 @@ const sidebars = {
       label: 'reSpeaker XVF3800 USB 4 Mic Array',
         items: [
         'Sensor/reSpeaker_XVF3800_USB_4_Mic_Array/respeaker_xvf3800_usb_4_mic_array',
-        'Sensor/reSpeaker_XVF3800_USB_4_Mic_Array/respeaker_xvf3800_with_xiao_intro',
+
+         {
+              type: 'category',
+              label: 'reSpeaker XVF3800 USB 4 Mic Array with XIAO',
+              items: [
+                  'Sensor/reSpeaker_XVF3800_USB_4_Mic_Array/respeaker_xvf3800_with_xiao_intro',
+              ],
+        },
+        
       ],
     },
 
